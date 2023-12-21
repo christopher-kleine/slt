898d40433261b86acdfac6f628ab000a8e89d1a0
Christopher Kleine <chris@suletuxe.de>
2023-12-21 15:21:51 +0100

    WIP

--- numbers/between.go
+++ numbers/between.go
@@ -1,9 +1,38 @@
 package numbers
 
 // Between takes a min and a max value, then returns a new function that takes an int.
-// The new functions returns true: e >= min && e < max
-func Between[E Number](min E, max E) func(E) bool {
+//
+// If lessOrEqual and moreOrEqual are true, the function return true if:
+// e >= min && e <= max
+//
+// If lessOrEqual is true, but moreOrEqual is false, the function return true if:
+// e >= min && e < max
+//
+// If lessOrEqual is false, but moreOrEqual is true, the function return true if:
+// e > min && e <= max
+//
+// If lessOrEqual and moreOrEqual are false, the function return true if:
+// e > min && e < max
+func Between[E Number](min E, max E, lessOrEqual, moreOrEqual bool) func(E) bool {
+	if lessOrEqual && moreOrEqual {
+		return func(e E) bool {
+			return e >= min && e <= max
+		}
+	}
+
+	if lessOrEqual {
+		return func(e E) bool {
+			return e >= min && e < max
+		}
+	}
+
+	if moreOrEqual {
+		return func(e E) bool {
+			return e > min && e <= max
+		}
+	}
+
 	return func(e E) bool {
-		return e >= min && e < max
+		return e > min && e < max
 	}
 }
